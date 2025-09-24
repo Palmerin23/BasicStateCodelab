586b0d1698e50513360733b85ee175526246cb4a
Palmerin23 <146683385+Palmerin23@users.noreply.github.com>
2025-09-23 21:15:28 -0600

    Se integro el comportamiento de quitar una tarea, pero primero hicimos la lista mutable

--- app/src/main/java/com/example/basicstatecodelab/ui/theme/WellnessTaskItem.kt
+++ app/src/main/java/com/example/basicstatecodelab/ui/theme/WellnessTaskItem.kt
@@ -11,6 +11,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Checkbox
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -43,13 +45,13 @@ fun WellnessTaskItem(
     }
 }
 @Composable
-fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier){
+fun WellnessTaskItem(taskName: String, onClose: () -> Unit,  modifier: Modifier = Modifier){
     var checkedState by rememberSaveable { mutableStateOf(false) }
     WellnessTaskItem(
         taskName = taskName,
         checked = checkedState,
         onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = {},
+        onClose = onClose,
         modifier = modifier,
 
         )
